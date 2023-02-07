f388039612bd7dbcbb80762aaf327e4642eb0a42
Tapas Mandal <alloymobileinc@gmail.com>
2023-02-07 09:38:16 -0500

    Added simple code

---
+++ src/main/java/com/alloymobile/helloworld/HelloWorld.java
@@ -0,0 +1,19 @@
+package com.alloymobile.helloworld;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Objects;
+
+@RestController
+public class HelloWorld {
+    @GetMapping(value="/greetme")
+    public String greetMe(@RequestParam(name="name", required = false)String name){
+        if(Objects.nonNull(name)){
+            return "Hello "+name;
+        }else{
+            return "Hello World";
+        }
+    }
+}
