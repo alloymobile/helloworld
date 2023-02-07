23b534da03128d6fea64a6f3fda00b71e31e5bcc
Tapas Mandal <alloymobileinc@gmail.com>
2023-02-07 09:55:08 -0500

    Added unit test

--- src/test/java/com/alloymobile/helloworld/HelloworldApplicationTests.java
+++ src/test/java/com/alloymobile/helloworld/HelloworldApplicationTests.java
@@ -2,12 +2,13 @@ package com.alloymobile.helloworld;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
 
 @SpringBootTest
 class HelloworldApplicationTests {
 
     @Test
-    void contextLoads() {
+    public void testName(){
+        Assert.isTrue(true,"True");
     }
-
 }
